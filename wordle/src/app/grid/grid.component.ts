d9fadec25da40744b8481b5fae20b6c2304c320d
akarafas <akarafas@live.unc.edu>
2022-01-22 15:48:23 -0500

    changing structure of grid to push blocks from ts

--- wordle/src/app/grid/grid.component.ts
+++ wordle/src/app/grid/grid.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { BlockComponent } from "C:/Users/Angel/IdeaProjects/wordle/wordle/src/app/grid/block/block.component";
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
@@ -7,16 +7,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridComponent implements OnInit {
   word: string = "wordl";
+  blocks: any[] = [];
+  submitCount: number;
+  wordFilled: boolean;
   constructor() {
+    this.submitCount = 0;
+    this.wordFilled = false;
+    for (let i = 0; i <30; i++) {
+      this.blocks.push(new BlockComponent());
+    }
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {  }
 
-  }
-      onKeypressEvent(event: any){
-         event.target.value = event.target.value.replace(/\W|\d/g, '').substr(0, 1).toUpperCase();
-         console.log(event.target.value);
-//          this.letter = event.target.value;
+  onKeypressEvent(event: any){
+     event.target.value = event.target.value.replace(/\W|\d/g, '').substr(0, 1).toUpperCase();
+     console.log(event.target.value);
+     if (this.wordFilled) {
+      if (this.submitCount == 0) {
+          console.log("5");
+
+      } else if (this.submitCount == 1) {
+          console.log("5");
+
+      } else if (this.submitCount == 2) {
+          console.log("5");
+
+      } else if (this.submitCount == 3) {
+          console.log("5");
+
+      } else if (this.submitCount == 4) {
+          console.log("5");
+
+      } else if (this.submitCount == 5) {
+          console.log("5");
       }
+     }
+//      this.letter = event.target.value;
+// set letter by replacing empty block with a new block with that letter
+// if backspace is pressed, replace block with new empty block
+
+  }
+  submitWord() {
+    this.submitCount+=1;
+  }
+
 
 }
