d1c8a38b7d96de70bd626583db4167812a993e46
akarafas <akarafas@live.unc.edu>
2022-01-22 10:39:41 -0500

    wordle app generated

---
+++ wordle/src/app/grid/grid.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.css']
+})
+export class GridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+// need to make a grid with 30 blocks
+
+}
